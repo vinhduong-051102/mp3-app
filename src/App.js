c523bd6cafc1853fca4255b165e2ea80d955698e
Dương Quý Vinh <Vinh@gmail.com>
2022-03-30 14:18:49 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useRef} from 'react'
+import './App.scss';
+import ReactTable from 'react-table';
+
 
 function App() {
+  const [job, setJob] = useState('')
+  const [jobs, setJobs] = useState([])
+  const inputRef = useRef()
+  const inputCurr = inputRef.current
+  const data = []
+  const handleAddJob = () => {
+    setJobs(prev => [...prev, job])
+    setJob('')
+    inputCurr.focus()
+  }
   return (
     <div className="App">
+      <div className="header">
+        <input
+          placeholder="Enter job"
+          onChange={(e) => setJob(e.target.value)}
+          value = {job}
+          ref={inputRef}
+         />
+        <button
+          onClick={handleAddJob}
+        >
+          Add job
+        </button>
+      </div>
       
     </div>
   );
